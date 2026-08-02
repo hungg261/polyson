e4b4740be9c7ae6db867555f018610aac2b3557d
hungg261 <phanthanhhung.26012010@gmail.com>
2026-08-02 20:42:06 +0700

    add extra arguments for compiler.py

--- polyson/compiler.py
+++ polyson/compiler.py
@@ -1,9 +1,13 @@
 import subprocess
 import sys
 
-def compile_cpp(file_name, output_name):
+def compile_cpp(file_name, output_name, extra_args=None):
     print(f"[*] Compiling {file_name}...")
-    cmd = ["g++", "-O2", file_name, "-o", output_name]
+    cmd = ["g++", "-O2"]
+    if extra_args:
+        cmd.extend(extra_args)
+    cmd.extend([file_name, "-o", output_name])
+    
     result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     if result.returncode != 0:
         print(f"[!] Compilation failed for {file_name}:\n{result.stderr}")
